dd11a8057dc828ba3cb65bfdab31fe753a62cc37
DeklanMA <deklanmalik@gmail.com>
2024-01-29 18:51:20 +0700

    create_LoginRegister-ModelandControllerPost

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::with('category', 'tags')->take(5)->latest()->get();
+    return view('page.home', compact('posts'));
+})->name('home');
+
+Route::view('about', 'page.about')->name('about');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__ . '/auth.php';
